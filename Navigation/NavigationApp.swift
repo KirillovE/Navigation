faf678a312be519ff7e1c77c6c4ba5a166d5f460
Eugène <Eugene.Kiriloff@iCloud.com>
2025-03-04 09:51:56 +0100

    Initial Commit

---
+++ Navigation/NavigationApp.swift
@@ -0,0 +1,17 @@
+//
+//  NavigationApp.swift
+//  Navigation
+//
+//  Created by Eugène Kiriloff on 04/03/2025.
+//
+
+import SwiftUI
+
+@main
+struct NavigationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
